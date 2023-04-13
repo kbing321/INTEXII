0eb674c9cb947389d645217955f2f45e69b8dbeb
judkins8 <joshua.judkins@gmail.com>
2023-04-13 14:50:19 -0600

    CRUD

--- Models/finalburialrecords2.cs
+++ Models/finalburialrecords2.cs
@@ -8,22 +8,20 @@ namespace INTEXII.Models
         public string colorvalue { get; set; }
         public string structurevalue { get; set; }
         public string sex { get; set; }
-        public decimal EstimateStature { get; set; }
-        public decimal depth { get; set; }
-        public decimal ageatdeath { get; set; }
+        public string depth { get; set; }
+        public string ageatdeath { get; set; }
         public string headdirection { get; set; }
-        public int burialid { get; set; }
+        public Int64? burialid { get; set; }
         public string textilefunctionvalue { get; set; }
         public string HairColor { get; set; }
         public string facebundles { get; set; }
         public string Notes { get; set; }
         public string filename { get; set; }
-        public decimal southtohead { get; set; }
-        public decimal westtohead { get; set; }
-        public decimal southtofeet { get; set; }
-        public decimal westtofeet { get; set; }
-        public decimal length { get; set; }
+        public string southtohead { get; set; }
+        public string westtohead { get; set; }
+        public string southtofeet { get; set; }
+        public string westtofeet { get; set; }
+        public string length { get; set; }
         public string Observations { get; set; }
-        public int idid { get; set; }
     }
 }
